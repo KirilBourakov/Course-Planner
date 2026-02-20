29b81d53158ac9130cadde348b2dfdc29e7d9937
Kiril <kirbou06012@gmail.com>
2026-02-19 20:56:31 -0400

    added course definition

--- course-planner/src/main/java/courseplanner/shared/Course.java
+++ course-planner/src/main/java/courseplanner/shared/Course.java
@@ -1,5 +1,26 @@
 package courseplanner.shared;
 
+import java.util.ArrayList;
+
 public class Course {
+	private String courseCode;
+	private String courseName;
+	private String description;
+	private int ch;
 	
+	
+	private ArrayList<String> tags;
+	private ArrayList<Course> prerequisites;
+	private ArrayList<Course> corequisites;
+	
+	Course(String courseCode, String courseName, String description, int ch){
+		this.courseCode = courseCode;
+		this.courseName = courseName;
+		this.description = description;
+		this.ch = ch;
+		
+		this.tags = new ArrayList<>();
+		this.prerequisites = new ArrayList<>();
+		this.corequisites = new ArrayList<>();
+	}
 }
